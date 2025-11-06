bbc4e058871f6b3d33c8a5b5f3eed54379036d37
Azeez <AOyelowo@smail.pembinatrails.ca>
2025-11-06 10:45:54 -0600

    New additions

--- bashing_box.sh
+++ bashing_box.sh
@@ -3,7 +3,7 @@
 default_objects=("Bandages" "Champagne" "Rope" "Festive sweater" "Mantle" "Soap" "SLeigh" "Snowman" "Bonfire" "Moccasin")
 
 default_objects_index=0
-#SHow entire list of objects function
+
 show_entire_list() {
 	echo "This is the full list"
 	for item in "${defualt_objects[@]}"; do
@@ -33,7 +33,7 @@ remove_last_item() {
 # Remove item at X position function
 remove_certain_item() {
 
-	read -p "Enter psoition to remove (0-${default_object[@]}" pos
+	read -p "Enter psoition to remove (${default_object[@]}" pos
 	if [[ $pos -ge 0 && $pos -lt ${default_objects[@]} ]]; then
 		unset ${default_objects[pos]}
 		default_objects=(${default_objects[@]})
@@ -42,11 +42,56 @@ remove_certain_item() {
 		echo "Invalid position"
 	fi
 }
+
+save_box() {
+	read -p "Enter a name for your box" name 
+	echo "${default_objects[@]}" > "data/$name.txt"
+	echo "Box saved as $name.txt in data/ folder"
+}
+
+load_box () {
+	read -p "ENter name of box you want to load" name 
+	if [[ "data/$name.txt" ]]; then
+		map -t default_objects < "data/$name.txt"
+		echo "Box $name was loaded successfully"
+	else
+		echo "The name of file doesn't exist in folder"
+	fi
+}
+
+list_existing_boxes
+	list_files=/home/azeezvbox/Bashing_Boxes/data
+	echo ""
+	echo "The existing boxes have been saved"
+	sleep 1
+	echo ""
+	ls $list_files
+}
+
+delete_saved_box() {
+	list_files=/home/azeezvbox/Bashing_Boxes/data
+	ls $list_files
+	echo ""
+	read -p "Enter the name of the file you would like to delete" answer
+	echo ""
+	rm $data/answer
+	echo "Your file $answer has been deleted"
+}
+
 # Exit function
 exit_game() {
-	echo "Goodbye, and thanks for playing!"
-	exit 0
+	read -p "Would yu like to save before exiting (yes/no):" answer
+	if [[ $answer == "y" ]]; then
+		save_box 
+		echo "You haved saved your box, thanks for playing!"
+		exit 0
+	else
+		echo "Aww man, it is okay that you don't want to save it, thanks for playing anyways"
+		exit 0
+	fi
+
 }
+
 
 echo "Current list of objects: ${default_objects[@]}"
 
@@ -76,7 +121,7 @@ read -p "What would you like to do? [1-10] " choice
 
 case $choice in
 	
-	1) show_entire_list 
+	1) show_entire_list
 		;;
 	2) print_certain_item 
 		;;
@@ -86,8 +131,17 @@ case $choice in
 		;;
 	5) remove_certain_item 
 		;;
-
-	6) exit_game 
+	6) save_box
+		;;
+	7) load_box
+		;;
+	8) list_existing_boxes
+		;;
+	9) delete_saved_box
+		;;
+	10) exit_game 
+		;;
+	*)
 		;;
 esac
 
